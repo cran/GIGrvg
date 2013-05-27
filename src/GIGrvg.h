19f41f46bf4418303554c6de3a9288699437b103
Josef Leydold <josef.leydold@wu.ac.at>
2013-05-27 00:00:00 +0000

    version 0.1

---
+++ src/GIGrvg.h
@@ -0,0 +1,10 @@
+
+SEXP rgig(SEXP sexp_n, SEXP sexp_lambda, SEXP sexp_chi, SEXP sexp_psi);
+/*---------------------------------------------------------------------------*/
+/* Draw sample from GIG distribution.                                        */
+/*---------------------------------------------------------------------------*/
+
+SEXP dgig(SEXP sexp_x, SEXP sexp_lambda, SEXP sexp_chi, SEXP sexp_psi, SEXP sexp_logvalue);
+/*---------------------------------------------------------------------------*/
+/* evaluate pdf of GIG distribution                                          */
+/*---------------------------------------------------------------------------*/
